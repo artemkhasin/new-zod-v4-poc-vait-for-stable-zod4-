c8093c6f753e5703619af156f84f36ac1845a17b
artemkhasin <artem.khasin@aizon.ai>
2025-05-08 14:36:50 +0200

    refactor: comment out unused resolvers and clean up submit button structure in GeneralForm

--- src/components/Form/GeneralForm.tsx
+++ src/components/Form/GeneralForm.tsx
@@ -1,5 +1,6 @@
 import { useForm, FormProvider } from "react-hook-form";
 // import { zodResolver } from "@hookform/resolvers/zod"; // Comment out or remove this line
+// import { standardSchemaResolver } from '@hookform/resolvers/standard-schema';
 import { zodV4Resolver } from "../../utils/zodV4Resolver"; // Import your custom resolver
 import { z } from "zod";
 import { Button, Stack } from "@mui/material";
@@ -17,6 +18,7 @@ const GeneralForm = (props: IGeneralFormProps) => {
     const methods = useForm({
         // @ts-expect-error: it is a custom resolver
         resolver: zodV4Resolver(formZodSchema), // Use your custom resolver here
+        // resolver: standardSchemaResolver(formZodSchema), // Use the standard schema resolver
         defaultValues: formConfig.defaultValues,
         mode: "onBlur",
     });
@@ -46,7 +48,13 @@ const GeneralForm = (props: IGeneralFormProps) => {
                             {renderInputField(field)}
                         </div>
                     ))}
-                    <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+                    <Button 
+                        type="submit" 
+                        variant="contained" 
+                        color="primary" 
+                        sx={{ mt: 2 }} 
+                        // onClick={onSubmit}
+                    >
                         Submit
                     </Button>
                 </Stack>
@@ -56,3 +64,13 @@ const GeneralForm = (props: IGeneralFormProps) => {
 };
 
 export default GeneralForm;
+
+// while official zodResolver is not supporting zod v4, we can use this custom resolver
+// import { zodResolver } from "@hookform/resolvers/zod";
+// or we can use standardSchemaResolver
+// import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
+
+// when warped in the <form> and set the required on inputs it will not allow to submit 
+// until all required inputs are filled
+// and only then it will do a validation
+// without form tags just putting onSubmit to button click will validate all inputs
